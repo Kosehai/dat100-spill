1a816e2336f0b12856ef2598189fbd7a20516410
wigumen <martin.wigum@gmail.com>
2021-10-18 15:18:17 +0200

    la til alt

---
+++ dat100-prosjekt-startkode-2021/src/no/hvl/dat100/prosjekt/modell/KortSamling.java
@@ -0,0 +1,174 @@
+package no.hvl.dat100.prosjekt.modell;
+
+import no.hvl.dat100.prosjekt.TODO;
+import no.hvl.dat100.prosjekt.kontroll.dommer.Regler;
+
+/**
+ * Struktur for å lagre ei samling kort. Kan lagre hele kortstokken. Det finnes
+ * en konstant i klassen Regler som angir antall kort i hver av de 4 fargene. Når
+ * programmet er ferdig settes denne til 13, men under utvikling / testing kan
+ * det være praktisk å ha denne mindre.
+ * 
+ */
+public class KortSamling {
+
+	private final int MAKS_KORT = 4 * Regler.MAKS_KORT_FARGE;
+
+	private Kort[] samling;
+	private int antall;
+
+	/**
+	 * Oppretter en tom Kortsamling med plass til MAKS_KORT (hele kortstokken).
+	 */
+	public KortSamling() {
+		samling = new Kort[MAKS_KORT];
+		antall = 0;
+	}
+
+	/**
+	 * Returnerer en tabell med kortene i samlinga. Tabellen trenger ikke være
+	 * full. Kortene ligger sammenhengende fra starten av tabellen. Kan få
+	 * tilgang til antallet ved å bruke metoden getAntallKort(). Metoden er
+	 * først og fremst ment å brukes i testklasser. Om man trenger
+	 * kortene utenfor, anbefales metoden getAlleKort().
+	 * 
+	 * @return tabell av kort.
+	 */
+	public Kort[] getSamling() {
+		return samling;
+	}
+	
+	/**
+	 * Antall kort i samlingen.
+	 * 
+	 * @return antall kort i samlinga.
+	 */
+	public int getAntalKort() {
+		return antall;
+	}
+	
+	/**
+	 * Sjekker om samlinga er tom.
+	 * 
+	 * @return true om samlinga er tom, false ellers.
+	 */
+	public boolean erTom() {
+		if(antall == 0) return true;
+		return false;
+	}
+
+	/**
+	 * Legg et kort til samlinga.
+	 * 
+	 * @param kort
+	 *            er kortet som skal leggast til.
+	 */
+	public void leggTil(Kort kort) {
+		samling[antall++] = kort;
+	}
+	
+	/**
+	 * Legger alle korta (hele kortstokken) til samlinga. Korta vil være sortert
+	 * slik at de normalt må stokkes før bruk.
+	 */
+	public void leggTilAlle() {
+		for(Kortfarge farge : Kortfarge.values()){
+			for(int i=1;i <= Regler.MAKS_KORT_FARGE; i++){
+				Kort kort = new Kort(farge, i);
+				leggTil(kort);
+			}
+		}
+	}
+
+	/**
+	 * Fjerner alle korta fra samlinga slik at den blir tom.
+	 */
+	public void fjernAlle() {
+		antall = 0;
+		samling = new Kort[MAKS_KORT];
+	}
+	
+	/**
+	 * Ser på siste kortet i samlinga.
+	 * 
+	 * @return siste kortet i samlinga, men det blir ikke fjernet. Dersom samalinga er tom, returneres
+	 *         null.
+	 */
+	public Kort seSiste() {
+		if(erTom()) return null;
+		return samling[antall-1];
+	}
+
+	/**
+	 * Tek ut siste kort fra samlinga.
+	 * 
+	 * @return siste kortet i samlinga. Dersom samalinga er tom, returneres
+	 *         null.
+	 */
+	public Kort taSiste() {
+		if(erTom()) return null;
+		antall--;
+		Kort kort = samling[antall];
+		samling[antall] = null;
+		return kort;
+	}
+	
+	/**
+	 * Undersøker om et kort finst i samlinga.
+	 * 
+	 * @param kort.
+	 * 
+	 * @return true om kortet finst i samlinga, false ellers.
+	 */
+	public boolean har(Kort kort) {
+		if(kort == null) return false;
+		for(Kort k : samling){
+			if(kort.equals(k)) return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Fjernar et kort frå samlinga. Dersom kortet ikke finnest i samlinga,
+	 * skjer ingenting med samilingen
+	 * 
+	 * @param kort
+	 *            kortet som skal fjernast. Dersom kortet ikke finnes, skjer
+	 *            ingenting.
+	 * @return true om kortet blev fjernet fra samlinga, false ellers.
+	 */
+			 
+	public boolean fjern(Kort kort) {
+		int rmindex = -1;
+		for(int i=0;i<antall;i++){
+			if(samling[i].equals(kort)){
+				rmindex = i;
+			}
+		}
+		if(rmindex == -1) return false;
+		Kort[] nysamling = new Kort[MAKS_KORT];
+		for(int i = 0, j = 0; i<samling.length; i++){
+			if(i != rmindex){
+				nysamling[j++] = samling[i];
+			}
+		}
+		samling = nysamling;
+		antall--;
+		return true;
+	}
+
+	/**
+	 * Gir kortene som en tabell av samme lengde som antall kort i samlingen
+	 * 
+	 * @return tabell av kort som er i samlingen, der kort skal ha samme rekkefølge
+	 *         som i kortsamlinga.
+	 */
+	public Kort[] getAllekort() {
+		Kort[] nysamling = new Kort[antall];
+		for(int i = 0;i < antall; i++){
+			nysamling[i] = samling[i];
+		}
+		return nysamling;
+	}
+	
+}
---
+++ dat100-prosjekt-startkode-2021/src/no/hvl/dat100/prosjekt/modell/KortUtils.java
@@ -0,0 +1,46 @@
+package no.hvl.dat100.prosjekt.modell;
+
+import java.util.Arrays;
+import java.util.Random;
+
+import no.hvl.dat100.prosjekt.TODO;
+
+public class KortUtils {
+
+	/**
+	 * Sorterer en samling. Rekkefølgen er bestemt av compareTo() i Kort-klassen.
+	 * 
+	 * @see Kort
+	 * 
+	 * @param samling
+	 * 			samling av kort som skal sorteres. 
+	 */
+	
+	public static void sorter(KortSamling samling) {
+		Kort[] nysamling = samling.getAllekort();
+		Arrays.sort(nysamling);
+		samling.fjernAlle();
+		for(Kort k : nysamling){
+			samling.leggTil(k);
+		}
+	}
+	
+	/**
+	 * Stokkar en kortsamling. 
+	 * 
+	 * @param samling
+	 * 			samling av kort som skal stokkes. 
+	 */
+	public static void stokk(KortSamling samling) {
+		Random rng = new Random();
+		Kort[] allekort = samling.getAllekort();
+		for(int i=0;i<allekort.length;i++){
+			int r = rng.nextInt(allekort.length);
+			//Swap
+			Kort kort = allekort[i];
+			allekort[i] = allekort[r];
+			allekort[r] = kort;
+		}
+	}
+	
+}
